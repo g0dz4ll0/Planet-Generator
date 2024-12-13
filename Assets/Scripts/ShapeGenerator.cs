11be58d28498c6300c800a45447e844dc3194dbc
Gonçalo Oliveira <goncalo.27@hotmail.com>
2024-12-13 20:10:19 +0000

    Planet Features - Implemented noise layers to add special features to the planets (mountains and continents)

--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -3,14 +3,40 @@ using UnityEngine;
 public class ShapeGenerator : MonoBehaviour
 {
     ShapeSettings _settings;
+    NoiseFilter[] _noiseFilters;
 
     public ShapeGenerator(ShapeSettings settings)
     {
         _settings = settings;
+        _noiseFilters = new NoiseFilter[settings.noiseLayers.Length];
+        for (int i = 0; i < _noiseFilters.Length; i++)
+        {
+            _noiseFilters[i] = new NoiseFilter(settings.noiseLayers[i].noiseSettings);
+        }
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
-        return pointOnUnitSphere * _settings.planetRadius;
+        float firstLayerValue = 0;
+        float elevation = 0;
+
+        if (_noiseFilters.Length > 0)
+        {
+            firstLayerValue = _noiseFilters[0].Evaluate(pointOnUnitSphere);
+            if (_settings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+
+        for (int i = 1; i < _noiseFilters.Length; i++)
+        {
+            if(_settings.noiseLayers[i].enabled)
+            {
+                float mask = (_settings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 1;
+                elevation += _noiseFilters[i].Evaluate(pointOnUnitSphere) * mask;
+            }  
+        }
+        return pointOnUnitSphere * _settings.planetRadius * (1 + elevation);
     }
 }
